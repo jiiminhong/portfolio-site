215984afe6c2a2425715623d500adfedc954fd0d
hongjimin00 <chunsim015@gmail.com>
2024-06-13 13:53:53 +0900

    feat: add main page grid

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,13 +1,24 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Main = () => {
   return (
-    <div className="w-auto h-auto">
-      <div>
-        <p className="font-bold text-2xl">안녕하세요</p>
-        <p className="font-bold text-2xl">개발자 홍지민입니다.</p>
-      </div>
-      <img src={process.env.PUBLIC_URL + "/images/yulmu.jpeg"} alt="" width="50%" height="100px" />
+    <div className="main-page">
+      <Container className="main-container">
+        <Row className="main-row h-auto">
+          <Col className="main-intro flex justify-center items-center">
+            <div className="main-text">
+              <p className="font-bold text-2xl">안녕하세요</p>
+              <p className="font-bold text-2xl">개발자 홍지민입니다.</p>
+            </div>
+
+            <div main-contact>dd</div>
+          </Col>
+          <Col className="main-img">
+            <img src={process.env.PUBLIC_URL + "/images/test.jpeg"} alt="" width="50%" />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
